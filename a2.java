994823f20ba768b67c41485c883aaaea7972c91d
Bhuvan-9999 <vvce22cseaiml0130@vvce.ac.in>
2025-03-18 12:36:50 +0530

    Add files via upload

---
+++ a2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class a2 {
+        public static void main (String[] args) {
+                int b=8;
+                int h=4;
+                double area = 0.5*b*h;
+                System.out.println("Area of Tringle is:"+area);
+        }
+}
